1fe7a2519e76c6ef82bae4529864d13b9e483795
k16025kk <ono@pluslab.org>
2023-08-21 21:10:38 +0900

    定数名修正

--- プロトタイプ/4WD_LapTimer_Proto/4WD_LapTimer_Proto.ino
+++ プロトタイプ/4WD_LapTimer_Proto/4WD_LapTimer_Proto.ino
@@ -1,8 +1,8 @@
 #include <Wire.h>
 
-const int SEG7_1_ADDR = 0x70;
-const int SEG7_2_ADDR = 0x74;
-const int SEG7_3_ADDR = 0x72;
+const int SEG7_0_ADDR = 0x70;
+const int SEG7_1_ADDR = 0x74;
+const int SEG7_2_ADDR = 0x72;
 
 const char seg[2][10] = {{0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f},  // 0~9 ドット無し
                          {0xBF,0x86,0xDB,0xCF,0xE6,0xED,0xFD,0x87,0xFF,0xEF}}; // 0~9 ドット有り
@@ -18,6 +18,17 @@ int disp_now;
 
 void seg7_driver_init(){
   Wire.begin();
+  Wire.beginTransmission(SEG7_0_ADDR);
+  Wire.write(0x21);
+  Wire.write(0x01);
+  Wire.endTransmission();
+  delay(10);
+  Wire.beginTransmission(SEG7_0_ADDR);
+  Wire.write(0x81);
+  Wire.write(0x01);
+  Wire.endTransmission();
+  delay(10);
+
   Wire.beginTransmission(SEG7_1_ADDR);
   Wire.write(0x21);
   Wire.write(0x01);
@@ -35,17 +46,6 @@ void seg7_driver_init(){
   Wire.endTransmission();
   delay(10);
   Wire.beginTransmission(SEG7_2_ADDR);
-  Wire.write(0x81);
-  Wire.write(0x01);
-  Wire.endTransmission();
-  delay(10);
-
-  Wire.beginTransmission(SEG7_3_ADDR);
-  Wire.write(0x21);
-  Wire.write(0x01);
-  Wire.endTransmission();
-  delay(10);
-  Wire.beginTransmission(SEG7_3_ADDR);
   Wire.write(0x81);
   Wire.write(0x01);
   Wire.endTransmission();
@@ -62,6 +62,12 @@ void loop() {
   for(int i = 0;i < DIG;i++)
   {
     disp_now = 0;
+    Wire.beginTransmission(SEG7_0_ADDR);
+    Wire.write(i*2);
+    Wire.write(seg[dot[disp_now][i]][disp[disp_now][i]]);
+    Wire.endTransmission();
+
+    disp_now++;
     Wire.beginTransmission(SEG7_1_ADDR);
     Wire.write(i*2);
     Wire.write(seg[dot[disp_now][i]][disp[disp_now][i]]);
@@ -69,12 +75,6 @@ void loop() {
 
     disp_now++;
     Wire.beginTransmission(SEG7_2_ADDR);
-    Wire.write(i*2);
-    Wire.write(seg[dot[disp_now][i]][disp[disp_now][i]]);
-    Wire.endTransmission();
-
-    disp_now++;
-    Wire.beginTransmission(SEG7_3_ADDR);
     Wire.write(i*2);
     Wire.write(seg[dot[disp_now][i]][disp[disp_now][i]]);
     Wire.endTransmission();
